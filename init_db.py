f5f1a76ac1691c1fa2d20b2840255482341c287d
Dimitry Snezhkov <dsnezhkov@gmail.com>
2016-11-01 12:40:03 -0500

    Rework layout

--- init_db.py
+++
@@ -1,3 +0,0 @@
-from models.tables import db
-db.drop_all()
-db.create_all()
--- scripts/start_server.sh
+++
@@ -1,1 +0,0 @@
-gunicorn app:app -p gunicorn.pid -b 0.0.0.0:8000  --error-logfile=gunicorn.log --access-logfile=gunicorn.log --capture-output  -D --timeout 120 --workers 2
